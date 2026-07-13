7e3e65da1e737a00e8ac96172dd6cf6d78a7c187
ApurupaVanteddu <apurupa.vanteddu@prodapt.com>
2026-07-13 22:46:12 +1300

    Create collection1.py

---
+++ day6/collection1.py
@@ -0,0 +1,10 @@
+#used to count occurances of elements
+
+'''
+data =['apple','banana','orange','apple','kiwi','banana','apple']
+'''
+
+from collections import Counter
+data =['apple','banana','orange','apple','kiwi','banana','apple']
+counter = Counter(data)
+print(counter)
